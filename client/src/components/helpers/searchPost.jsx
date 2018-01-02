fcdb263f38613d75fdcfe7ec7c6814db8ae3e404
Derrick Cross <decross1@me.com>
2018-01-02 13:49:22 -0800

    Wrote searchPost function

---
+++ client/src/components/helpers/searchPost.jsx
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+var React = require('react');
+
+var searchPost = (username) => {
+  var data = JSON.stringify({username: username})
+  $.ajax({
+    url: 'server url here', 
+    type: 'POST', 
+    dataType: 'text',
+    data: data, 
+    success: (data) => {
+      console.log('Post success', data);
+    }, 
+    error: (data) => {
+      console.log('Post error', data);
+    }
+  })
+}
+
+export default searchPost;
