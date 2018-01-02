d0550ea5eaa9e6fcde4c50a7d6cb05ef6478599f
Derrick Cross <decross1@me.com>
2018-01-02 12:31:44 -0800

    Created inital mongoose schema model

--- database/index.js
+++ database/index.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: { type: Number, required: true, unique: true }
+  name: { type: String, required: true }
+  full_name: type: String, require: true }
+  owner: {
+    login: String,
+    id: Number,
+    url: String,
+    html_url: String,
+    },
+  private: Boolean,
+  html_url: String,
+  description: String,
+  url: String,
+  created_at: Date,
+  forks_count: Number,
+  default_branch: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
