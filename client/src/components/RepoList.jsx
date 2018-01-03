ec4f511bc4c6e32a5883ee0f6392211c56840b3a
Derrick Cross <decross1@me.com>
2018-01-02 23:14:48 -0800

    Small change to url in github list

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -5,7 +5,7 @@ const RepoList = (props) => (
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
     {props.repos.map((repo, index) => {
-     return <li key={repo.id}>Repo: <a href={repo.url}>{repo.name}</a>   ||   Username: {repo.owner.login}   ||   Total Forks: {repo.forks_count} </li>
+     return <li key={repo.id}>Repo: <a href={repo.html_url}>{repo.name}</a>   ||   Username: {repo.owner.login}   ||   Total Forks: {repo.forks_count} </li>
    })}
 
   </div>
